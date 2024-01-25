6bac9374c22c6d58ba2031e5ebb7a14a8e414a83
Max <maximesin21@gmail.com>
2024-01-25 14:58:47 +0100

    Add observer pattern

---
+++ Patterns/Observer/index.ts
@@ -0,0 +1,53 @@
+interface Observer {
+  update(subject: Subject): void;
+}
+
+interface Subject {
+  attach(observer: Observer): void;
+  detach(observer: Observer): void;
+  notify(): void;
+}
+
+class Lead {
+  constructor(public name: string, public phone: string) {}
+}
+
+class NewLead implements Subject {
+  private observers: Observer[] = [];
+  public state: Lead;
+
+  attach(observer: Observer): void {
+    if (this.observers.includes(observer)) {
+      return;
+    }
+    this.observers.push(observer);
+  }
+
+  detach(observer: Observer): void {
+    const observerIndex = this.observers.indexOf(observer);
+    if (observerIndex == -1) {
+      return;
+    }
+    this.observers.splice(observerIndex, 1);
+  }
+
+  notify(): void {
+    for (const observer of this.observers) {
+      observer.update(this);
+    }
+  }
+}
+
+class NotificationService implements Observer {
+  update(subject: Subject): void {
+    console.log('Service got a notification');
+    console.log(subject);
+  }
+}
+
+class LeadService implements Observer {
+  update(subject: Subject): void {
+    console.log('Service got a notification');
+    console.log(subject);
+  }
+}
